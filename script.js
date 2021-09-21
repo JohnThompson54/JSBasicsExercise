58dd6248a3898cfc009c9188146dde36aa20a60e
Your Name <you@example.com>
2021-09-21 08:24:18 -0500

    exercise

---
+++ script.js
@@ -0,0 +1,40 @@
+// 1
+console.log(9);
+
+// 2
+// numbers, strings, booleans, null, forward
+
+// 3
+// +, -, *, /, **, %
+
+// 4
+console.log(5%2);
+console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINS Y\'ALL');
+
+//6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("John Thompson".length);
+
+// 8
+console.log("Avengers: Endgame" [16]);
+
+// BONUS
+
+// 1
+console.log('\\');
+
+// 2
+console.log(5**4-9);
+
+//3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+// 3b
+console.log(("I".length + "Am".length) * "Iron".length / "Man".length%4);
+
+// 4
